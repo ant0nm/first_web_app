349209f4323dfb68bc596553b8fe7c2516aed69d
Anton <anton.mois33v@gmail.com>
2019-02-28 16:42:48 -0500

    Added 2 more links

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -28,13 +28,27 @@ def home_page(request):
     return HttpResponse(response)
 
 def portfolio_page(request):
-    random_number = randint(0,100)
-    image_url = "https://picsum.photos/400/600/?image={}".format(random_number)
-    context = {'gallery_image': image_url}
+    image_urls = []
+    for i in range(5):
+        random_number = randint(0,100)
+        image_urls.append("https://picsum.photos/400/600/?image={}".format(random_number))
+    context = {'gallery_images': image_urls}
     reposnse = render(request, 'gallery.html', context)
+    return HttpResponse(reposnse)
+
+def about_me_page(request):
+    context = {'skills': ['HTML', 'CSS', 'python', 'django'], 'interests': ['psychology', 'neuroscience', 'math', 'running']}
+    response = render(request, 'about_me.html', context)
+    return HttpResponse(response)
+
+def favourites_page(request):
+    context = {'fave_links': ['https://www.reddit.com/', 'https://www.goodreads.com/', 'https://www.vice.com/en_ca']}
+    reposnse = render(request, 'favourites.html', context)
     return HttpResponse(reposnse)
 
 urlpatterns = [
     path('home/', home_page),
-    path('portfolio/', portfolio_page)
+    path('portfolio/', portfolio_page),
+    path('about_me/', about_me_page),
+    path('favourites/', favourites_page)
 ]
